6d1723b174138bbcd9cdd863989f8c438716e67b
Antoine <aaltorffer@orange.fr>
2025-01-27 18:39:51 +0100

    Add access to debug log from error

--- glslang/src/error.rs
+++ glslang/src/error.rs
@@ -3,21 +3,42 @@ use crate::shader::Target;
 use crate::GlslProfile;
 use thiserror::Error;
 
+/// The error logs
+#[derive(Debug)]
+pub struct GlslangErrorLog {
+    pub log: String,
+    pub debug_log: String,
+}
+
+impl GlslangErrorLog {
+    pub fn new(log: String, debug_log: String) -> Self {
+        Self {
+            log, debug_log
+        }
+    }
+}
+
+impl std::fmt::Display for GlslangErrorLog {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Log:\n{}\nDebugLog:\n{}", self.log, self.debug_log)
+    }
+}
+
 /// The error type for `glslang`.
 #[derive(Debug, Error)]
 pub enum GlslangError {
     /// Error occurred when preprocessing.
     #[error("preprocess error: {0}")]
-    PreprocessError(String),
+    PreprocessError(GlslangErrorLog),
     /// Error occurred when preprocessing.
     #[error("parse error: {0}")]
-    ParseError(String),
+    ParseError(GlslangErrorLog),
     /// Error occurred when mapping IO.
     #[error("map io error: {0}")]
-    MapIoError(String),
+    MapIoError(GlslangErrorLog),
     /// Error occurred when linking
     #[error("program link error: {0}")]
-    LinkError(String),
+    LinkError(GlslangErrorLog),
     /// The shader stage was not found in the program.
     #[error("shader stage not found: {0:?}")]
     ShaderStageNotFound(ShaderStage),
